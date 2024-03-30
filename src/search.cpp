66b60aa44b060926d4b488e033a7a5a86de94ec4
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-03-30 11:57:01 +0100

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -867,7 +867,6 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-}
         Depth R = std::min(int(eval - beta) / 144, 6) + depth / 3 + 4;
 
         ss->currentMove         = MOVE_NULL;
