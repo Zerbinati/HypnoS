6403cab4a4b2cb783741f78ec5a535575add5a6e
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-04-02 14:45:40 +0200

    Take 3
    XInTheDark author

--- src/search.cpp
+++ src/search.cpp
@@ -1344,7 +1344,9 @@ moves_loop:  // When in check, search starts here
                     // We make sure to limit the extensions in some way to avoid a search explosion
                     if (!PvNode && ss->multipleExtensions <= 16)
                     {
-                        extension = 2 + (value < singularBeta - 22 && !ttCapture);
+                        extension = 2;
+                        if (!ttCapture)
+                            extension += (value < singularBeta - 50) + (value < singularBeta - 120);
                         depth += depth < 14;
                     }
                     if (PvNode && !ttCapture && ss->multipleExtensions <= 5
