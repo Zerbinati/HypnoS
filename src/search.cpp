8c3ac002ca5be5f43dbe9fc9516ecc18b891e080
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-07-05 02:19:38 +0200

    less reduction for cutnodes: 3000
    Ergodice Authored

--- src/search.cpp
+++ src/search.cpp
@@ -1428,7 +1428,7 @@ moves_loop:  // When in check, search starts here
                       + (*contHist[3])[movedPiece][to_sq(move)] - 3817;
 
         // Decrease/increase reduction for moves with a good/bad history (~8 Elo)
-        r -= ss->statScore / 13659;
+        r -= (ss->statScore + 3000 * cutNode) / 13659;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
         if (depth >= 2 && moveCount > 1 + rootNode)
