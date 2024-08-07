9d372fb32836d3231abc44d2d382b5d7f41cd9d6
Zerbinati <zerbinati.marco@gmail.com>
2024-08-07 08:16:39 +0200

    take2

--- src/search.cpp
+++ src/search.cpp
@@ -1469,7 +1469,7 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result
                 // was good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 47 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
+                const bool doShallowerSearch = value < bestValue + newDepth + 10 * (depth < 12);  // (~2 Elo)
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
