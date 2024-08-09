8197e1950199ab127e24f17651796e036952519d
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-08-09 16:37:52 +0200

    Fix usage of abs vs std::abs
    xu-shawn authored

--- src/search.cpp
+++ src/search.cpp
@@ -1590,7 +1590,7 @@ moves_loop:  // When in check, search starts here
                 else
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
-                    if (depth > 2 && depth < 13 && abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
+                    if (depth > 2 && depth < 13 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
                         depth -= 1 + ss->ttPv;
 
                     assert(depth > 0);
