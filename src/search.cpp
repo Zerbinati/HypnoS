603326c352a9a7cdd37087043a93a187ed6df1fa
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-08-09 15:40:14 +0200

    Add extra bonuses to some moves that forced a fail low
    The previous patch on this idea was giving bonuses to this moves if best value of search is far below current static evaluation.
    This patch does similar thing but adds extra bonus when best value of search is far below static evaluation before previous move.
    
    Vizvezdenec authored

--- src/search.cpp
+++ src/search.cpp
@@ -1639,8 +1639,8 @@ moves_loop:  // When in check, search starts here
     else if (!priorCapture && prevSq != SQ_NONE)
     {
         int bonus = (depth > 5) + (PvNode || cutNode) + ((ss - 1)->statScore < -14963)
-                  + ((ss - 1)->moveCount > 11)
-                  + (!ss->inCheck && bestValue <= ss->staticEval - 150);
+                  + ((ss - 1)->moveCount > 11) + (!ss->inCheck && bestValue <= ss->staticEval - 142)
+                  + (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 77);
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus);
         thisThread->mainHistory[~us][from_to((ss - 1)->currentMove)]
