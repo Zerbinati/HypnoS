a03f0b2e0314ca46ed8305fc89898482e1e86f1d
Marco Zerbinati <zerbinati.marco@gmail.com>
2024-08-09 11:20:08 +0200

    Tweak the stats bonus and malus
    For depth 1 we don't have a negative score anymore.
    
    FauziAkram authored

--- src/search.cpp
+++ src/search.cpp
@@ -109,10 +109,10 @@ constexpr Value to_static_eval(const Value v) {
 }
 
 // History and stats update bonus, based on depth
-int stat_bonus(Depth d) { return std::min(223 * d - 332, 1258); }
+int stat_bonus(Depth d) { return std::clamp(245 * d - 320, 0, 1296); }
 
 // History and stats update malus, based on depth
-int stat_malus(Depth d) { return std::min(536 * d - 299, 1353); }
+int stat_malus(Depth d) { return (d < 4 ? 554 * d - 303 : 1203); }
 
 // Add a small random component to draw evaluations to avoid 3-fold blindness
 Value value_draw(const Thread* thisThread) {
