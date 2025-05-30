8cdfa4d18b1b43a0fd0f7cc67bd51fa262037e84
Zerbinati <zerbinati.marco@gmail.com>
2025-05-30 21:14:38 +0200

    Update evaluate.h

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,8 +39,8 @@ int calculate_hypnos_default_bonus(const Position& pos);
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-1c0000000000.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+// #define EvalFileDefaultNameBig "nn-1c0000000000.nnue"
+// #define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
 
 namespace NNUE {
 struct Networks;
